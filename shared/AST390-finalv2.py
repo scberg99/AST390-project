c986f7113b73cbd099f8b1facdd01156163948e2
scberg99 <stephenberg99@gmail.com>
2020-05-04 15:13:20 -0400

    Added dictionary of semi-major axis values

--- shared/AST390-finalv2.py
+++ shared/AST390-finalv2.py
@@ -3,17 +3,20 @@ import matplotlib; matplotlib.use("pdf")
 import matplotlib.pyplot as plt
 import numpy as np
 
+a_vals = {'Mercury':0.387,'Venus':0.723336,'Earth':1,'Mars':1.52371,
+          'Jupiter':5.2029,'Saturn':9.583,'Uranus':19.189,'Neptune':30.0699}
+
 # create simulation object
 sim = rebound.Simulation()
 sim.add(m=1) # Sun
-sim.add(m=1.66e-30, a=0.387, e=0.205) # Mercury
-sim.add(m=2.447e-6, a=0.723336, e=0.00678) # Venus
-sim.add(m=3e-6, a=1.0, e=0.017) #Earth
-sim.add(m=3.212e-7, a=1.52371, e=0.09339) # Mars
-sim.add(m=9.543e-4,a=5.2029,e=0.0484) # Jupiter
-sim.add(m=2.856e-4,a=9.583,e=0.0565) #Saturn
-sim.add(m=4.372e-5,a=19.189,e=0.04726) # Uranus
-sim.add(m=5.178e-5,a=30.0699,e=0.00859) # Neptune
+sim.add(m=1.66e-30, a=a_vals["Mercury"], e=0.205) # Mercury
+sim.add(m=2.447e-6, a=a_vals["Venus"], e=0.00678) # Venus
+sim.add(m=3e-6, a=a_vals["Earth"], e=0.017) #Earth
+sim.add(m=3.212e-7, a=a_vals["Mars"], e=0.09339) # Mars
+sim.add(m=9.543e-4,a=a_vals["Jupiter"],e=0.0484) # Jupiter
+sim.add(m=2.856e-4,a=a_vals["Saturn"],e=0.0565) #Saturn
+sim.add(m=4.372e-5,a=a_vals["Uranus"],e=0.04726) # Uranus
+sim.add(m=5.178e-5,a=a_vals["Neptune"],e=0.00859) # Neptune
 
 sim.move_to_com()
 sim.status()
@@ -29,9 +32,8 @@ eccen_nep = []
 times = []
 
 
-for i in range(101):
-    tmax=1.0e2*i
-    sim.integrate(tmax)
+for step in np.linspace(0,40000.*np.pi,1000): # 20000 orbits
+    sim.integrate(step)
     times.append(sim.t)
     eccen_merc.append(sim.particles[1].e)
     eccen_venus.append(sim.particles[2].e)
@@ -42,15 +44,16 @@ for i in range(101):
     eccen_ur.append(sim.particles[7].e)
     eccen_nep.append(sim.particles[8].e)
 
-plt.plot(times, eccen_merc)
-plt.plot(times, eccen_venus)
-plt.plot(times, eccen_earth)
-plt.plot(times, eccen_mars)
-plt.plot(times, eccen_jup)
-plt.plot(times, eccen_sat)
-plt.plot(times, eccen_ur)
-plt.plot(times, eccen_nep)
-
+merc = plt.plot(times, eccen_merc, label='mercury')
+venus = plt.plot(times, eccen_venus, label='venus')
+earth = plt.plot(times, eccen_earth, label='earth')
+mars = plt.plot(times, eccen_mars, label='mars')
+jupiter = plt.plot(times, eccen_jup, label='jupiter')
+saturn = plt.plot(times, eccen_sat, label='saturn')
+uranus = plt.plot(times, eccen_ur, label='uranus')
+neptune = plt.plot(times, eccen_nep, label='neptune')
+plt.legend()
 plt.xlabel('Time (unit)')
 plt.ylabel('Eccentricity')
+
 plt.savefig("orbits.pdf")
