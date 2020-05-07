043da991826fe17df72de398a2c705c22783537d
scberg99 <stephenberg99@gmail.com>
2020-05-07 15:06:33 -0400

    simplified control flow with function definitions

--- shared/AST390-finalv2.py
+++ shared/AST390-finalv2.py
@@ -3,38 +3,7 @@ import matplotlib; matplotlib.use("pdf")
 import matplotlib.pyplot as plt
 import numpy as np
 
-a_vals = {'Mercury':0.387,'Venus':0.723336,'Earth':1,'Mars':1.52371,
-          'Jupiter':5.2029,'Saturn':9.583,'Uranus':19.189,'Neptune':30.0699}
-
-# create simulation object
-sim = rebound.Simulation()
-sim.add(m=1) # Sun
-sim.add(m=1.66e-30, a=a_vals["Mercury"], e=0.205) # Mercury
-sim.add(m=2.447e-6, a=a_vals["Venus"], e=0.00678) # Venus
-sim.add(m=3e-6, a=a_vals["Earth"], e=0.017) #Earth
-sim.add(m=3.212e-7, a=a_vals["Mars"], e=0.09339) # Mars
-sim.add(m=9.543e-4,a=a_vals["Jupiter"],e=0.0484) # Jupiter
-sim.add(m=2.856e-4,a=a_vals["Saturn"],e=0.0565) #Saturn
-sim.add(m=4.372e-5,a=a_vals["Uranus"],e=0.04726) # Uranus
-sim.add(m=5.178e-5,a=a_vals["Neptune"],e=0.00859) # Neptune
-
-sim.move_to_com()
-sim.status()
-
-eccen_merc = []
-eccen_venus = []
-eccen_earth = []
-eccen_mars = []
-eccen_jup = []
-eccen_sat = []
-eccen_ur = []
-eccen_nep = []
-times = []
-
-
-for step in np.linspace(0,40000.*np.pi,1000): # 20000 orbits
-    sim.integrate(step)
-    times.append(sim.t)
+def get_e_vals():
     eccen_merc.append(sim.particles[1].e)
     eccen_venus.append(sim.particles[2].e)
     eccen_earth.append(sim.particles[3].e)
@@ -44,16 +13,53 @@ for step in np.linspace(0,40000.*np.pi,1000): # 20000 orbits
     eccen_ur.append(sim.particles[7].e)
     eccen_nep.append(sim.particles[8].e)
 
-merc = plt.plot(times, eccen_merc, label='mercury')
-venus = plt.plot(times, eccen_venus, label='venus')
-earth = plt.plot(times, eccen_earth, label='earth')
-mars = plt.plot(times, eccen_mars, label='mars')
-jupiter = plt.plot(times, eccen_jup, label='jupiter')
-saturn = plt.plot(times, eccen_sat, label='saturn')
-uranus = plt.plot(times, eccen_ur, label='uranus')
-neptune = plt.plot(times, eccen_nep, label='neptune')
-plt.legend()
-plt.xlabel('Time (unit)')
-plt.ylabel('Eccentricity')
+def plot_e_vals():
+    merc = plt.plot(times, eccen_merc, label='mercury')
+    venus = plt.plot(times, eccen_venus, label='venus')
+    earth = plt.plot(times, eccen_earth, label='earth')
+    mars = plt.plot(times, eccen_mars, label='mars')
+    jupiter = plt.plot(times, eccen_jup, label='jupiter')
+    saturn = plt.plot(times, eccen_sat, label='saturn')
+    uranus = plt.plot(times, eccen_ur, label='uranus')
+    neptune = plt.plot(times, eccen_nep, label='neptune')
+    plt.legend()
+    plt.title('Orbital Eccentricities')
+    plt.xlabel('Time (years)')
+    plt.ylabel('Eccentricity')
+    plt.savefig("eccentricity.pdf")
 
-plt.savefig("orbits.pdf")
+a_vals = {'Mercury':0.387,'Venus':0.723336,'Earth':1,'Mars':1.52371,
+          'Jupiter':5.2029,'Saturn':9.583,'Uranus':19.189,'Neptune':30.0699}
+
+# create simulation object
+sim = rebound.Simulation()
+sim.add(m=1) # Sun
+sim.add(m=1.66e-30, a=a_vals["Mercury"], e=0.205) # Mercury
+sim.add(m=2.447e-6, a=a_vals["Venus"], e=0.00678) # Venus
+sim.add(m=3e-6, a=a_vals["Earth"], e=0.017) # Earth
+sim.add(m=3.212e-7, a=a_vals["Mars"], e=0.09339) # Mars
+sim.add(m=9.543e-4,a=a_vals["Jupiter"], e=0.0484) # Jupiter
+sim.add(m=2.856e-4,a=a_vals["Saturn"],e=0.0565) # Saturn
+sim.add(m=4.372e-5,a=a_vals["Uranus"],e=0.04726) # Uranus
+sim.add(m=5.178e-5,a=a_vals["Neptune"],e=0.00859) # Neptune
+
+sim.move_to_com()
+sim.integrator = 'whfast'
+
+eccen_merc = [] # Eccentricity of mercury orbit
+eccen_venus = []
+eccen_earth = []
+eccen_mars = []
+eccen_jup = []
+eccen_sat = []
+eccen_ur = []
+eccen_nep = []
+
+times = []
+
+for step in np.linspace(0,100000,100): # 20000 orbits
+    sim.integrate(step)
+    times.append(sim.t)
+    get_e_vals()
+
+plot_e_vals()
